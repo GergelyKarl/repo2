64b0da216b35e9391fd27adcebaddcacc2539e9f
GergelyKarl <karl.ger@hotmail.com>
2020-12-10 13:20:59 +0100

    "first"
    	new file:   cart/README.md

---
+++ project-with-sanity.io/src/App.js
@@ -0,0 +1,27 @@
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+  <BrowserRouter>
+  <NavBar />
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/about" component={About} />
+    <Route path="/post/:slug" component={SinglePost} />
+    <Route path="/post" component={Post} />
+    <Route path="/project" component={Project} />
+  </Switch>
+  
+  </BrowserRouter>
+  
+    )
+}
+
+export default App;
---
+++ project-with-sanity.io/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import image from "../ih.jpg";
+
+function Home() {
+  return (
+    <main>
+      <img src={image} alt="hhgh" className="absolute object-cover w-full h-full" />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8" >
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Vmi</h1>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
