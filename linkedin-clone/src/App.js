96fca346138c886247177f0c7f69187877fa667d
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-01-03 18:27:35 +0100

    v1.5

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -4,18 +4,25 @@ import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import Feed from "./Feed";
-
-//2:34
+import { selectUser } from "./features/userSlice";
+import {useSelector} from "react-redux";
+import Login from "./Login"
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      <div className="app__body">
-        <SideBar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <SideBar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
