5025acc2a2e5bee66822cba31fd670a281f78cca
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-01-16 21:52:52 +0100

    v0.2

--- tesla-homepage/src/App.js
+++ tesla-homepage/src/App.js
@@ -1,10 +1,57 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
+
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Item from "./components/Item";
 
 function App() {
   return (
     <div className="App">
-   <Header />
+      <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title="asd"
+          desc="asdsa"
+          descLink="qweqe"
+          backgroundImg={SolarPanels}
+          leftBtnLink=""
+          leftBtnText=""
+          rightBtnText=""
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="asd"
+          desc="asdsa"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnLink=""
+          leftBtnText=""
+          rightBtnText=""
+          rightBtnLink=""
+          twoButtons="true"
+          
+        />
+        <Item
+          title="asd"
+          desc="asdsa"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnLink=""
+          leftBtnText=""
+          rightBtnText=""
+          rightBtnLink=""
+          twoButtons="true"
+          
+        />
+      </div>
     </div>
   );
 }
