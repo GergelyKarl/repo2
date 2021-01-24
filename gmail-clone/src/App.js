4254b77dc2ccde9c16bd15a85faae41fbc15fb95
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-01-24 22:46:09 +0100

    v0.2

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+//1.07
 function App() {
   return (
     <div className="app">
       <Header />
       <Sidebar />
-<h1>asd</h1>
+      <h1>asd</h1>
     </div>
   );
 }
--- gmail-clone/src/components/Sidebar.js
+++ gmail-clone/src/components/Sidebar.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import "./Sidebar.css"
-const Sidebar = () => {
-    return (
-        <div className="sidebar">
-           
-        </div>
-    )
-}
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import SendIcon from "@material-ui/icons/Send";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-export default Sidebar
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar__compose"
+      >
+        Compose
+      </Button>
+
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={1458}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={15} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={15} />
+      <SidebarOption Icon={LabelImportantIcon} title="Imported" number={15} />
+      <SidebarOption Icon={SendIcon} title="Sent" number={15} />
+      <SidebarOption Icon={DescriptionIcon} title="Drafts" number={15} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" />
+    </div>
+  );
+};
+
+export default Sidebar;
