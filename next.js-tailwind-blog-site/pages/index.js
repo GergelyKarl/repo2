d084642295533fb54eba91a7afe6acd56fc59e9c
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-03-01 22:42:42 +0100

    cleanup

---
+++ next.js-tailwind-blog-site/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>My Blog</h1>
+      </main>
+    </div>
+  );
+}
