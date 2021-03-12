a26610fc0ae8cc48e12275bd6df443e870b304dc
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-03-12 23:10:58 +0100

    cleanup

---
+++ project-to-my-portfolio-5/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Kripto Kereső</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+    </div>
+  )
+}
