b7d9c95f05d4ea1e3b3b357ab46ac020596161da
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-03-14 22:06:07 +0100

    Update index.js

--- project-to-my-portfolio-5/pages/index.js
+++ project-to-my-portfolio-5/pages/index.js
@@ -1,15 +1,28 @@
-import Head from 'next/head'
-
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Kripto Kereső</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
+      <SearchBar type="text" placeholder="Gépelj a szűkítéshez..." />
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=huf&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  );
+
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins,
+    },
+  };
+};
