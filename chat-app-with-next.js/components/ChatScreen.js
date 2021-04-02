33bf5ee366a08eb6e7feff09a21be8273e63532e
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-04-02 23:41:01 +0200

    0.7

---
+++ chat-app-with-next.js/components/ChatScreen.js
@@ -0,0 +1,97 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import styled from "styled-components";
+import { auth, db } from "../firebase";
+import { useRouter } from "next/router";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import { Avatar, IconButton } from "@material-ui/core";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Message from "./Message";
+
+
+const ChatScreen = ({ chat, messages }) => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
+
+  return (
+    <Container>
+      <Header>
+        <Avatar></Avatar>
+        <HeaderInformation>
+          <h3>Rec email</h3>
+          <p>Last seen...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+
+      <MessageContainer>
+          {showMessages()}
+        <EndofMessage />
+      </MessageContainer>
+    </Container>
+  );
+};
+
+export default ChatScreen;
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div``;
+
+const EndofMessage = styled.div``;
