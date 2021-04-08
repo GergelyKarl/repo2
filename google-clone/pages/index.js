7ee1f387bdbe7d0a93afbc513f7973f034355714
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-04-08 16:38:06 +0200

    added tailwind

---
+++ google-clone/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+<h1>asd</h1>
+    </div>
+  )
+}
