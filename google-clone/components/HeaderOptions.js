c0b3172c5aa3872fea42a98ec04c29791d0d3a06
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-04-14 15:51:23 +0200

    0.7

--- google-clone/components/HeaderOptions.js
+++ google-clone/components/HeaderOptions.js
@@ -1,8 +1,26 @@
+import HeaderOption from "./HeaderOption";
+import {
+  DotsVerticalIcon,
+  MapIcon,
+  NewspaperIcon,
+  PhotographIcon,
+  PlayIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
+
 const HeaderOptions = () => {
-  //2:03
+  
   return (
-    <div>
-      <div>
+    <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 pl-52 lg:border-b-[1px]" >
+      <div className="flex space-x-6">
+        <HeaderOption title="Összes" selected Icon={SearchIcon} />
+        <HeaderOption title="Képek" Icon={PhotographIcon} />
+        <HeaderOption title="Videók" Icon={PlayIcon} />
+        <HeaderOption title="Hírek" Icon={NewspaperIcon} />
+        <HeaderOption title="Térkép" Icon={MapIcon} />
+        <HeaderOption title="Egyebek" Icon={DotsVerticalIcon} />
+      </div>
+      <div className="flex space-x-4 cursor-pointer">
         <p>Beállítások</p>
         <p>Eszközök</p>
       </div>
