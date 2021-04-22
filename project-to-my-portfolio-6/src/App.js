10e3e4e8468e704063c0650551fd4932f953e619
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-04-22 10:10:06 +0200

    cleanup

---
+++ project-to-my-portfolio-6/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+`;
+const Nav = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 99;
+  background-color: #f0f3f7;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
