5034212650c15547860f41b114deaea54f3ed4f8
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-05-19 19:59:59 +0200

    done

--- birthday-countdown/app.js
+++ birthday-countdown/app.js
@@ -1,5 +1,5 @@
 const timeLeft = document.getElementById("time-left");
-const birthday = new Date("1986-01-21");
+const birthday = new Date("2022-01-21");
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -8,14 +8,28 @@ const day = hour * 24;
 let timeId;
 function countDown() {
   const today = new Date();
-  const timeSpan = today - birthday;
-  console.log(timeSpan);
+  const timeSpan = birthday - today;
 
   if (timeSpan <= 0) {
     timeLeft.innerHTML = "HBD";
     clearInterval(timeId);
-    return
+    return;
   }
+
+  const days = Math.floor(timeSpan / day);
+  const hours = Math.floor((timeSpan % day) / hour);
+  const minutes = Math.floor((timeSpan % hour) / minute);
+  const seconds = Math.floor((timeSpan % minute) / second);
+
+  timeLeft.innerHTML =
+    days +
+    " days " +
+    hours +
+    " hours " +
+    minutes +
+    " minutes " +
+    seconds +
+    " seconds ";
 }
 
 timeId = setInterval(countDown, second);
