e323ee19945b8fdd53773eabb6caadae1a6dd1f6
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-06-17 22:05:09 +0200

    0.2 routes/controllers

--- mern-crud/backend/index.js
+++ mern-crud/backend/index.js
@@ -2,15 +2,16 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import router from "./routes/routes.js";
 
 const app = express();
-
+app.use("/posts", router);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const connectString =
-  "mongodb+srv://************@cluster0.bv6dt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://admin:qweqweqwe@cluster0.bv6dt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
   .connect(connectString, {
@@ -18,4 +19,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => app.listen(4000, () => console.log("connected")))
-  .catch();
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
