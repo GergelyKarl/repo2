28857c5c92b85ed1d64dcb1d19a0e1dccae848ed
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-06-18 22:52:08 +0200

    0.3 jsx+styling

--- mern-crud/frontend/src/App.js
+++ mern-crud/frontend/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import pic from "./images/pic.jpg";
+import Post from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles"
 
 const App = () => {
-    return (
-        <div>
-            <h1>sd</h1>
-        </div>
-    )
-}
+const classes=useStyles()
 
-export default App
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Blog
+        </Typography>
+        <img className={classes.image} src={pic} alt="blog" height="60" width="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing>
+            <Grid item xs={12} sm={7}>
+              <Post />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
---
+++ mern-crud/frontend/src/components/Posts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from "./styles"
+const Post = () => {
+    const classes=useStyles()
+    return (
+        <div>
+            <h1>ghfh</h1>
+        </div>
+    )
+}
+
+export default Post
