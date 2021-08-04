ec873c412ce10fcf873d2c4c6c8df290745be734
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-08-04 17:43:39 +0200

    0.1 init

---
+++ slack-clone/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header"
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+           <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
