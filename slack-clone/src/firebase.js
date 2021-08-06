9bab5aa4ac4763805d435b539fc4253d3c662eee
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-08-06 18:33:30 +0200

    0.2 Header done

--- slack-clone/src/firebase.js
+++ slack-clone/src/firebase.js
@@ -1,4 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  *****************
+  apiKey: "AIzaSyC3bNW7jyaLoiWIV5zGlTAH32jXMYfMAIU",
+  authDomain: "slack-clone-ffe16.firebaseapp.com",
+  projectId: "slack-clone-ffe16",
+  storageBucket: "slack-clone-ffe16.appspot.com",
+  messagingSenderId: "1054001778142",
+  appId: "1:1054001778142:web:1e76bda6a7a661b98ee14f",
+  measurementId: "G-05K2FF5Y7Y",
 };
