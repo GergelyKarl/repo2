8d42e0c42f522c6510c74111bb8da3204a261421
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-09-04 18:07:43 +0200

    0.3 angular project init

---
+++ spring+angular-crud/crud-client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public employees?: Employee[];
+  constructor(private employeeService: EmployeeService) {
+
+
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
