0c42b2f87c1cfbeac0a828ab4bf56c85f3e312b9
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-09-21 16:55:40 +0200

    0.1 tsconfig+cleanup

---
+++ legyen-on-is/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+<div className="main">
+
+</div>
+<div className="piramis"></div>
+    </div>
+  );
+}
+
+export default App;
